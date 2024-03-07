ef6ce1f244a75899db3c50b1243237a2ad894644
DaMao <blitecorful@gmail.com>
2024-03-07 10:25:30 +0800

    在循环的判断中增加了重启DMA接收的语句

--- Core/Src/cppstart.cpp
+++ Core/Src/cppstart.cpp
@@ -9,6 +9,7 @@ void cpp_start(){
     {
         RFID1.receivedDataProcessing();
         receiveEndFlag = 0;
+        HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rxBuffer, RXBUFFER_SIZE); //restart the DMA receiving process
         print_to_USB(RFID1.return_databuffer_address());
     }
 }
