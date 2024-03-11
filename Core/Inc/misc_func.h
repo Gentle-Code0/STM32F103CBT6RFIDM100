e2ddc162d2648bfd6ea9385914511acce314ecbb
DaMao <blitecorful@gmail.com>
2024-03-11 15:41:55 +0800

    用户rx中断事件回调函数的初步编写

--- Core/Inc/misc_func.h
+++ Core/Inc/misc_func.h
@@ -7,6 +7,8 @@ extern "C" {
 
 #include <stdint.h>
 #include "usbd_cdc_if.h"
+#include "cppstart.h"
+
 
 //Copy a source array to a destination array
 //TO DO: This function should output error message when length is larger than any of these arrays.
@@ -15,6 +17,10 @@ void copy_array(uint8_t* src, uint8_t* des, uint8_t length);
 //A print to USB function
 //!Be aware, it contains a 500ms delay!
 void print_to_USB(uint8_t* message);
+
+void register_callback_init(UART_HandleTypeDef* huart);
+
+void user_rx_callback(struct __UART_HandleTypeDef *huart, uint16_t Pos);
 
 #ifdef __cplusplus
 }
--- Core/Src/misc_func.cpp
+++ Core/Src/misc_func.cpp
@@ -15,3 +15,27 @@ void print_to_USB(uint8_t* message)
     CDC_Transmit_FS(message, Length);
     HAL_Delay(500); //delay for half a second
 }
+
+//Register user's callback function
+void register_callback_init(UART_HandleTypeDef* huart)
+{
+    HAL_UART_RegisterRxEventCallback(huart, user_rx_callback);
+}
+
+//For idle interrupt, pos is dmabuffer size minus remained damabuffer space(RxCounter)
+void user_rx_callback(struct __UART_HandleTypeDef *huart, uint16_t Pos)
+{
+    if(huart->Instance == USART1)
+    {
+        //RxEventType decides what kind of interrupt is triggered
+        //There are 3 types of event, half receive complete, full receive complete and idle event
+        //Because RFID module return data size is predictable,
+        //we only deal with idle event.
+        //For half reveive complete and full receive complete,
+        //one can write seperate code for processing
+        if(huart->RxEventType == HAL_UART_RXEVENT_IDLE) 
+        {
+
+        }
+    }
+}
